0105e2b6b17dde4fe75887e23268f5680bf675fe
Daivagna Nanavati <dnanavati@horizontalintegration.com>
2023-10-29 11:33:01 +0530

    Pushed Card component

--- JssApp-Fed/src/components/CardGrid.tsx
+++ JssApp-Fed/src/components/CardGrid.tsx
@@ -1,6 +1,12 @@
 //console.log('data is' + props);
 
-import { Field, ImageField, Image as JssImage, NextImage } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  Field,
+  ImageField,
+  Image as JssImage,
+  NextImage,
+  RichText,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 
 export type CardFields = {
   fields?: {
@@ -27,17 +33,19 @@ const CardList = (props: CardListProps): JSX.Element => {
       {/* <!-- This is an example component --> */}
 
       <div className="px-3 lg:max-w-6xl mx-auto">
-        <div className="text-4xl sm:text-5xl text-center my-10">What type of food do you like?</div>
+        <div className="text-4xl sm:text-5xl text-center my-10">
+          {props.rendering.fields.Heading.value}
+        </div>
         <div className="grid grid-cols-3 gap-3 auto-rows-max">
           {list.map((fields: any, index) => {
-            console.log('FIeld name' + JSON.stringify(fields.fields.CardIcon.value));
+            console.log('FIeld name' + JSON.stringify(fields.fields.CardImage.value));
 
             return (
               <div key="1" className=" max-w-5xl m-auto ">
                 <div className="bg-white">
                   <div>
-                    <NextImage
-                      field={fields.fields.CardIcon}
+                    <JssImage
+                      field={fields.fields.CardImage}
                       width={50}
                       height={50}
                       className="m-auto"
@@ -45,15 +53,14 @@ const CardList = (props: CardListProps): JSX.Element => {
                   </div>
                   <div className="px-10 py-6 mb-10 text-center flex flex-col">
                     <div className="text-2xl font-bold text-purple-500 mb-4 mt-auto">
-                      {fields.fields.CardText.value}
+                      {fields.fields.Heading.value}
                     </div>
-                    <span className="text-sm">{fields.fields.CardDescription.value}</span>
+                    <RichText field={fields.fields.BodyCopy} className="text-sm" />
                   </div>
                 </div>
               </div>
             );
           })}
-          ;
         </div>
       </div>
     </>
