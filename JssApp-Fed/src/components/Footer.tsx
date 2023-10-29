cb25da641852021714516af3524738a75a099631
Nikhil Raj <92802817+Nikhil-rajs@users.noreply.github.com>
2023-10-29 10:41:02 +0530

    footer-css

--- JssApp-Fed/src/components/Footer.tsx
+++ JssApp-Fed/src/components/Footer.tsx
@@ -18,13 +18,13 @@ export const Default = (props: FooterProps): JSX.Element => {
   if (props.fields) {
     return (
       <>
-        <div className="footer bg-indigo-300">
+        <div className="footer bg-cyan-100">
           <div className="px-4 sm:px-8 text-center">
-            <JssRichText field={props.fields.Description}></JssRichText>
-            <div className="social-container">
+            <JssRichText className="text-[1.5rem] mb-8 font-bold " field={props.fields.Description}></JssRichText>
+            <div className="social-container flex justify-around pb-1 ">
               {props.fields?.SocialIcon.map((fields: any, index) => (
-                <span className="fa-stack" key={index}>
-                  <Link href={fields.fields.Link?.value.href}>
+                <span className="fa-stack " key={index}>
+                  <Link className='text-black font-medium' href={fields.fields.Link?.value.href}>
                     <i className="fas fa-circle fa-stack-2x"></i>
                     <i className={fields.fields.FontAwesomeClass?.fields.ClassName.value}></i>
                     {fields.fields.Title?.value}
@@ -34,15 +34,15 @@ export const Default = (props: FooterProps): JSX.Element => {
             </div>
           </div>
         </div>
-        <div className="copyright bg-indigo-300">
+        <div className="copyright bg-cyan-100">
           <div className="px-4 sm:px-8 lg:grid lg:grid-cols-1">
-            <p className="pb-2 p-small statement text-center">
+            <p className="pb-2 p-small statement text-center font-normal">
               Copyright ©
-              <a href="#your-link" className="no-underline text-black">
+              <a href="#your-link" className="no-underline text-black font-normal">
                 {new Date().getFullYear()}
               </a>
             </p>
-            <ul className="mb-4 list-unstyled p-small px-20 flex lg:grid-cols-2">
+            <ul className="mb-0 list-unstyled p-small px-20 flex md:justify-center font-semibold lg:grid-cols-2">
               <li className="mb-2 pr-2.5">
                 <a
                   href={props.fields.TermsAndConditionLink?.value.href}
@@ -55,7 +55,7 @@ export const Default = (props: FooterProps): JSX.Element => {
               <li className="mb-2">
                 <Link
                   href={props.fields.PrivarcyPolicyLink?.value.href}
-                  className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                  className="text-blue-600 hover:text-blue-800 visited:text-purple-600 no-underline"
                 >
                   {props.fields.PrivarcyPolicyTitle?.value}
                 </Link>
