11e728c82ecf2443e4367625ced410b19ac94da9
Nikhil Raj <nikhilraj786786@gmail.com>
2023-10-29 04:14:22 -0700

    Team Member Field Update

--- JssApp-Fed/src/components/Header.tsx
+++ JssApp-Fed/src/components/Header.tsx
@@ -19,7 +19,7 @@ const HeaderDefaultComponent = (props: HeaderProps): JSX.Element => (
 export const Default = (props: HeaderProps): JSX.Element => {
   if (props.fields) {
     return (
-      <nav className="bg-gray-800">
+      <nav className="bg-cyan-100 border-b border-black">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             {/* <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -57,7 +57,7 @@ export const Default = (props: HeaderProps): JSX.Element => {
                 </svg>
               </button>
             </div> */}
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
               <div className="flex flex-shrink-0 items-center">
                 <JssLink field={props.fields?.LogoLink}>
                   <JssImage className="h-8 w-auto" field={props.fields?.Logo} />
@@ -77,7 +77,7 @@ export const Default = (props: HeaderProps): JSX.Element => {
                                 return (
                                   <JssLink
                                     field={subLink.fields?.Link as LinkField}
-                                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                                    className="text-gray-800 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                                   >
                                     {subLink.fields?.Title.value}
                                   </JssLink>
@@ -90,7 +90,7 @@ export const Default = (props: HeaderProps): JSX.Element => {
                         <>
                           <JssLink
                             field={link.fields?.Link as LinkField}
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                            className="text-gray-800 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                           >
                             {link.fields?.Title.value}
                           </JssLink>
